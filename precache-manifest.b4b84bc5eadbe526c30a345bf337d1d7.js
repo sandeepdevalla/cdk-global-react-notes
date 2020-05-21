5b47c6f14ab47b480e60316d527de7cbabb8542a
Sandeep Devalla <sandeepdevalla@Sandeeps-MacBook-Pro.local>
2020-05-21 19:09:54 +0530

    Updates

---
+++ precache-manifest.b4b84bc5eadbe526c30a345bf337d1d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dbe83e86f52be495d2544c60b64ef94",
+    "url": "/cdk-global-react-notes/index.html"
+  },
+  {
+    "revision": "6750b16708f0d207dd60",
+    "url": "/cdk-global-react-notes/static/css/main.5b6cac40.chunk.css"
+  },
+  {
+    "revision": "b5d626062a7fd99fc574",
+    "url": "/cdk-global-react-notes/static/js/2.d50bbbb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cdk-global-react-notes/static/js/2.d50bbbb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6750b16708f0d207dd60",
+    "url": "/cdk-global-react-notes/static/js/main.671d7ebf.chunk.js"
+  },
+  {
+    "revision": "90636c11e217888542a8",
+    "url": "/cdk-global-react-notes/static/js/runtime-main.3fd27d1a.js"
+  }
+]);
